d5232c8c611431f40a6e5d60e5bba3d3d8bfa9d0
Jamie Egan <j.egan@mac.com>
2020-12-04 15:45:05 +0000

    updating app with shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
 import './App.css';
 import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shop/shop.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
