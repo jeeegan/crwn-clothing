6b60197c6e1b1952ece19c9402c1a8f4521c0a6f
Jamie Egan <j.egan@mac.com>
2020-12-08 13:27:20 +0000

    updating application with relect, replaced cart selectors with memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import {connect} from 'react-redux'
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selectors.js';
 
 import './cart-dropdown.styles.scss';
 
@@ -17,8 +18,8 @@ const CartDropdown = ({cartItems}) => (
   </div>
 );
 
-const matchStateToProps = ({cart: {cartItems}}) => ({
-  cartItems
+const matchStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(matchStateToProps)(CartDropdown);
