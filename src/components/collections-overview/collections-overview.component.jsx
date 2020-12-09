633c4ac4e2c3892ff1cfeb38fe825ef8bd1bf08e
Jamie Egan <j.egan@mac.com>
2020-12-09 12:32:01 +0000

    new redux flow for directory/shop

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import { selectShopCollections } from '../../redux/shop/shop.selectors.js';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors.js';
 
 import CollectionPreview from '../collection-preview/collection-preview.component.jsx';
 import './collections-overview.styles.scss';
@@ -17,7 +17,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectShopCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
