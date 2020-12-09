f70fb39038e494cd5263f97e4ab38e81d2c7db44
Jamie Egan <j.egan@mac.com>
2020-12-09 12:05:56 +0000

    added collection pages

--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,5 +1,7 @@
 import SHOP_DATA from './shop.data';
-const INITIAL_STATE = SHOP_DATA;
+const INITIAL_STATE = {
+  collections: SHOP_DATA
+};
 
 const shopReducer = (state=INITIAL_STATE, action) => {
   switch(action.type) {
