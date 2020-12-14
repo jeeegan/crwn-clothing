784e8407abc00f2ba8857152aea096cdf1c04100
Jamie Egan <j.egan@mac.com>
2020-12-14 16:26:17 +0000

    corrected bug with reloading of collection page

--- src/page/shop/shop.component.jsx
+++ src/page/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import {createStructuredSelector} from 'reselect';
 import {connect} from 'react-redux';
 
 import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
-import {selectIsCollectionFetching} from '../../redux/shop/shop.selectors';
+import {selectIsCollectionFetching, selectIsCollectionsLoaded} from '../../redux/shop/shop.selectors';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
@@ -21,7 +21,7 @@ class ShopPage extends React.Component {
     }
     
     render() {
-      const {match, isCollectionFetching} = this.props;
+      const {match, isCollectionFetching, isCollectionsLoaded} = this.props;
       return(
         <div className='shop-page'>
           <Route
@@ -30,7 +30,7 @@ class ShopPage extends React.Component {
           />
           <Route
             path={`${match.path}/:collectionId`}
-            render={props => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>}
+            render={props => <CollectionPageWithSpinner isLoading={!isCollectionsLoaded} {...props}/>}
           />
         </div>
       );
@@ -39,7 +39,8 @@ class ShopPage extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionsLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
